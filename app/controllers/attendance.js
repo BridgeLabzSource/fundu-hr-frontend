f17e6b0a03a1b52e36c59e5d6be0bcf2862c148c
pranalipatil209 <pranalipatil209@gmail.com>
2016-06-24 18:31:48 +0530

    timeout

--- app/controllers/attendance.js
+++ app/controllers/attendance.js
@@ -1,11 +1,15 @@
-angular.module('attendanceApp').controller('attendanceCtrl',['$scope', function($scope){
+angular.module('attendanceApp').controller('attendanceCtrl',['$scope','$timeout',function($scope,$timeout){
     $scope.isActive = true;
     $scope.showLog = true;
     $scope.showLo = function(){
         if($scope.name.toLowerCase() == "log >"){
-            $scope.showLog = false;
+            $timeout(function() {
+                $scope.showLog = false;
+            }, 1000);
         }else if($scope.name.toLowerCase() == "clear"){
-            $scope.showLog = true;
+            $timeout(function() {
+                $scope.showLog = true;
+            }, 800);
         }
     };
 }]);
