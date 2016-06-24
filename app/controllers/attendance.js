c90451fae92d715f060869b068b7a7a77704127f
pranalipatil209 <pranalipatil209@gmail.com>
2016-06-24 17:24:53 +0530

    log displayed

--- app/controllers/attendance.js
+++ app/controllers/attendance.js
@@ -1,3 +1,11 @@
 angular.module('attendanceApp').controller('attendanceCtrl',['$scope', function($scope){
     $scope.isActive = true;
+    $scope.showLog = true;
+    $scope.showLo = function(){
+        if($scope.name.toLowerCase() == "log >"){
+            $scope.showLog = false;
+        }else if($scope.name.toLowerCase() == "clear"){
+            $scope.showLog = true;
+        }
+    };
 }]);
