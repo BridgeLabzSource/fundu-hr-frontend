375d672f6d041fee5b5deed5c3061dc6b8ccaf88
pranalipatil209 <pranalipatil209@gmail.com>
2016-07-26 10:45:15 +0530

    error handled

--- app/directive/auto-manager.js
+++ app/directive/auto-manager.js
@@ -19,7 +19,7 @@ angular.module('attendanceApp').directive('autoManager', function (localStorageS
         controller: function ($rootScope, $scope) {
 
             $rootScope.data = localStorageService.get('dict');
-            console.log();
+                console.log($rootScope.data);
         },
         link: function ($scope, element) {
             var input = '';
